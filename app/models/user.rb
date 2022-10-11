3db7b0980b5d1963ce349fe2ebeb408228e06fd0
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-10-11 20:56:12 +0300

    resource generations

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    has_secure_password
+    has_many :reviews
+    has_many :books, through: :reviews
+    validates :username, presence: true, uniqueness: true
+end
