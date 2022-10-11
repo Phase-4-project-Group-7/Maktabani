f8148a9b18c995eac2a01a5649aad6ca584ddeb1
Kelvin <kmwendwa476@gmail.com>
2022-10-12 00:27:40 +0300

    added reviews actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,20 @@
 class ReviewsController < ApplicationController
+
+  def create
+    review = @current_user.review.create!(:review_params)
+    render json: review, status: :created
+  end
+
+  def update
+    review = find(params[:id])
+    review.update!(:user_params)
+    render json: review, status: :accepted
+  end
+
+  def destroy
+    review = find(params[:id])
+    review.destroy
+    head :no_content
+  end
+
 end
