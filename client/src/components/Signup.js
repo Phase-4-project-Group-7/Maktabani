38774676bbdb663304a77bc7e0624e8f3d730131
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-10-12 21:05:01 +0300

    add the messages  to be output

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -41,11 +41,50 @@ const Signup = () => {
         setError(false);
       }
     };
-    
 
+    // success message
+    const successMessage = () => {
+      return (
+        <div className='success' style={{display:submit ? '' :'none'}}>
+            <h2>{name} signed up successfully </h2>
+        </div>
+      );
+    };
+
+    // error message
+    const errorMessage = () => {
+      return (
+         <div className='error' style= {{display: error ? '' : 'none',}}>
+            <h2>Enter all the fields</h2>
+      </div>
+       )
+      };
 
   return (
-    <div>
+    <div className='signup'>
+
+      <div>
+        <h1>Signup Form</h1>
+      </div>
+      
+      <div className='messages' >
+        {errorMessage()}
+        {successMessage()}
+
+      </div>
+
+      <form>
+        <label className="label">Name</label>
+        <input onChange={handleName} className= "input" value={name} type="text"/>
+
+        <label className="label">Email</label>
+        <input onChange={handleEmail} className= "input" value={email} type="email"/>
+
+        <label className="label">Password</label>
+        <input onChange={handlePassword} className= "input" value={password} type="password"/>
+
+        <button onClick={handleSubmit} className="btn" type="submit">Submit</button>
+      </form>
       
     </div>
   )
