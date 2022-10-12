df15508c3b002adb47e387303264109b9a39920b
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-10-13 00:16:41 +0300

    Adding post route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :reviews, only: [:destroy, :update, :show]
+  resources :reviews, only: [:destroy, :update, :show, :create]
   resources :books, only: [:index, :show, :create]
 
   post '/signup', to: 'users#create'
