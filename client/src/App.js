3c6902dd38c6ad6442a8d052044c5bccea8e6012
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-10-12 21:05:16 +0300

    add signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Signup/>
     </div>
   );
 }
