5b3e9d69be6c470354283cdfa237d5fe9b09cb90
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-10-13 12:43:13 +0300

    import browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
+
 import Navbar from './components/Navbar';
 import Signup from './components/Signup';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navbar/>
+
       <Signup/>
+
+      </Router>
+   
     </div>
   );
 }
