0973e0bb261f0769ee0394b1a7b5aa999f40db8d
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-10-14 01:51:32 +0300

    add importations to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,56 @@
-import React from 'react';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
 import Navbar from './components/Navbar';
+import Login from "./components/Login";
 import Signup from './components/Signup';
-import { BrowserRouter as Router } from 'react-router-dom';
+import HomePage from "./components/HomePage";
+import Header from "./components/Header";
+import { Route, BrowserRouter as Router, BrowserRouter } from 'react-router-dom';
+
+
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+  
   return (
-    <div className="App">
-      <Router>
-      <Navbar/>
+    <>
+       <Router>
+          <Navbar user={user} setUser={setUser} />
+          <Header/>
 
-      <Signup/>
+          <main>
+            {user ? (
+              <Route path="/">
+                <HomePage user={user} /> 
+              </Route>
+            
+           ) : (
+             <BrowserRouter>
+                <Route path="/signup">
+                   <Signup setUser={setUser} />
+                </Route>
+                <Route path="/login">
+                  <Login setUser={setUser} />
+                </Route>
+                <Route path="/">
+                    <HomePage />
+                </Route>
+          </BrowserRouter>
+           )}
 
-      </Router>
-   
-    </div>
+
+          </main>
+
+       </Router>
+    
+    </>
+
   );
 }
 
