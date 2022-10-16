4be5903585bcebcbd0470f65860e2d0a6e485029
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-10-16 20:32:02 +0300

    Homepage routing

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   
-  get '/k',
+  get '/',
   to: 'fallback#index',
   constraints: ->(req) { !req.xhr? && req.format.html? }
 end
